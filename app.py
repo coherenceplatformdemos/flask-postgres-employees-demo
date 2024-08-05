0f50e55e3b870069ade924365af95425d1c972de
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-08-05 16:48:15 +0200

    add EMPLOYEES_DATABASE_URL

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('EMPLOYEES_DATABASE_URL')
 db = SQLAlchemy(app)
 
 
